cf674d8cbb39d14290bbfc6734acc0c696ea8a99
edug2923 <edug2923@gmail.com>
2022-04-30 16:36:10 +0200

    He metido la clase niño

--- CampamentoInfantil/src/main/java/Threads/Niño.java
+++ CampamentoInfantil/src/main/java/Threads/Niño.java
@@ -8,7 +8,41 @@ package Threads;
 /**
  *
  * @author Francisco
- */
-public class Niño {
+ */   
+public class Niño extends Thread {
+    private int contador=0;
+    private String id;
+
+    public Niño() {
+        this.id=generaNombre();
+    }
+    
+    public String generaNombre(){
+        String id;
+        int n=(int) (Math.random()*10000);
+        if (n<10){
+            Integer.toString(n);
+            id="N000"+n;
+        }else if(n<100){
+            Integer.toString(n);
+            id="N00"+n;
+        }else if(n<1000){
+            Integer.toString(n);
+            id="N0"+n;
+        }else{
+            Integer.toString(n);
+            id="N"+n;
+        }
+        return id;
+    }
+
+    public int getContador() {
+        return contador;
+    }     
+    
+    public void run(){
+        
+    }
+    
     
 }
